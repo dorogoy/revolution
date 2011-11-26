8adc85d5573bf2d49ec6a9ebc6c8283961072ea5
Sergio Rael <sergio.rael@seitic.com>
2011-11-26 09:58:55 +0100

    added dashboards.inc.php to spanish

---
+++ core/lexicon/es/dashboards.inc.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Spanish language strings for Dashboards
+ *
+ * @package modx
+ * @subpackage lexicon
+ * @language es
+ */
+$_lang['dashboard'] = 'Panel';
+$_lang['dashboard_add'] = 'Añadir Panel';
+$_lang['dashboard_create'] = 'Crear Panel';
+$_lang['dashboard_desc_name'] = 'El nombre del Panel.';
+$_lang['dashboard_desc_description'] = 'Una descripción corta del Panel.';
+$_lang['dashboard_desc_hide_trees'] = 'Seleccionar esto ocultará el árbol de la izquierda cuando este panel se procese en la página de bienvenida.';
+$_lang['dashboard_hide_trees'] = 'Ocultar Árbol de la Izquierda';
+$_lang['dashboard_duplicate'] = 'Duplicar Panel';
+$_lang['dashboard_remove'] = 'Borrar Panel';
+$_lang['dashboard_remove_confirm'] = '¿Está seguro de querer eliminar este Panel?';
+$_lang['dashboard_update'] = 'Actualizar Panel';
+$_lang['dashboard_err_ae_name'] = '¡Un panel con el mismo nombre "[[+name]]" ya existe! Por favor, inténtelo con otro nombre.';
+$_lang['dashboard_err_duplicate'] = 'Ocurrió un error al tratar de duplicar el panel.';
+$_lang['dashboard_err_nf'] = 'Panel no encontrado.';
+$_lang['dashboard_err_ns'] = 'Panel no especificado.';
+$_lang['dashboard_err_ns_name'] = 'Por favor, especifique un nombre para este widget.';
+$_lang['dashboard_err_remove'] = 'Ocurrió un error al tratar de eliminar este Panel.';
+$_lang['dashboard_err_remove_default'] = '¡No puede eliminar el panel por defecto!';
+$_lang['dashboard_err_save'] = 'Ocurrió un error al tratar de guardar el panel.';
+$_lang['dashboard_usergroup_add'] = 'Asignar Panel a Grupo de Usuarios';
+$_lang['dashboard_usergroup_remove'] = 'Eliminar Panel de Grupo de Usuarios';
+$_lang['dashboard_usergroup_remove_confirm'] = '¿Seguro que desea que este grupo de usuarios vuelva a utilizar el panel predeterminado?';
+$_lang['dashboard_usergroups.intro_msg'] = 'El siguiente es un listado de todos los grupos de usuario que utilizan este Panel.';
+$_lang['dashboard_widget_err_placed'] = '¡El widget ya está colocado en este panel!';
+$_lang['dashboard_widgets.intro_msg'] = 'Aquí puede agregar, administrar y eliminar widgets de este panel. También puede arrastrar y colocar las filas en la cuadrícula para reoganizarlos';
+$_lang['dashboards'] = 'Paneles';
+$_lang['dashboards.intro_msg'] = 'Aquí puede administrar todos los paneles disponibles para este administrador de MODX.';
+$_lang['rank'] = 'Rango';
+$_lang['user_group_filter'] = 'Por Grupo de Usuario';
+$_lang['widget'] = 'Widget';
+$_lang['widget_content'] = 'Contenido del Widget';
+$_lang['widget_create'] = 'Crear Nuevo Widget';
+$_lang['widget_err_ae_name'] = '¡Ya existe un widget con el nombre "[[+name]]"! Por favor, inténtelo con otro nombre.';
+$_lang['widget_err_nf'] = '¡Widget no encontrado!';
+$_lang['widget_err_ns'] = '¡Widget no especificado!';
+$_lang['widget_err_ns_name'] = 'Por favor, especifique un nombre para el widget.';
+$_lang['widget_err_remove'] = 'Ocurrió un error al tratar de eliminar el Widget.';
+$_lang['widget_err_save'] = 'Ocurrió un error al tratar de guardar el Widget.';
+$_lang['widget_file'] = 'Fichero';
+$_lang['widget_dashboards.intro_msg'] = 'A continuación hay una lista de todos los paneles en los que se ha colocado este Widget.';
+$_lang['widget_dashboard_remove'] = 'Eliminar Widget del Panel';
+$_lang['widget_description_desc'] = 'Una descripción, o una clave de entrada en el léxico, del Widget y qué hace.';
+$_lang['widget_html'] = 'HTML';
+$_lang['widget_lexicon_desc'] = 'El tema del léxico que hay que cargar con este Widget. Útil para proporcionar traducciones para el nombre y la descripción, así como cualquier texto en el Widget.';
+$_lang['widget_name_desc'] = 'El nombre, o la Clave de la entrada en el léxico, del Widget.';
+$_lang['widget_new'] = 'Nuevo Widget';
+$_lang['widget_remove'] = 'Eliminar Widget';
+$_lang['widget_remove_confirm'] = '¿Está seguro de querer eliminar este Widget de panel? Esto es permanente y eliminará el Widget de todos los paneles.';
+$_lang['widget_remove_multiple'] = 'Eliminar múltiples Widgets';
+$_lang['widget_remove_multiple_confirm'] = '¿Está seguro de querer eliminar estos Widgets de panel? Esto es permanente y eliminará los Widgets de todos los paneles a los que estén asignados.';
+$_lang['widget_namespace'] = 'Espacio de Nombres';
+$_lang['widget_namespace_desc'] = 'El Espacio de Nombres en el que este Widget será cargado. Útil para rutas personalizadas.';
+$_lang['widget_php'] = 'Widget PHP en línea';
+$_lang['widget_place'] = 'Colocar Widget';
+$_lang['widget_size'] = 'Tamaño';
+$_lang['widget_size_desc'] = 'El tamaño del widget. Puede ser de un ancho de media pantalla ("Medio"), del tamaño de la pantalla ("Completo"), o del ancho de la pantalla completa y dos filas ("Doble").';
+$_lang['widget_size_double'] = 'Doble';
+$_lang['widget_size_full'] = 'Completo';
+$_lang['widget_size_half'] = 'Medio';
+$_lang['widget_snippet'] = 'Snippet';
+$_lang['widget_type'] = 'Tipo de Widget';
+$_lang['widget_type_desc'] = 'El tipo de widget que puede ser: los widgets "Snippet" son snippets de MODX que se ejecutan y retornan su salida; los widgets "HTML" son simplemente HTML plano; Los widgets "Fichero" se cargan directamente desde ficheros, que pueden retornar su salida o el nombre de la clase modDashboardWidgetClass-extended a cargar; los widgets "PHP en línea" son widgets con PHP directamente en su contenido, similares a los Snippets.';
+$_lang['widget_unplace'] = 'Eliminar Widget';
+$_lang['widget_update'] = 'Actualizar Widget';
+$_lang['widgets'] = 'Widgets';
+$_lang['widgets.intro_msg'] = 'A continuación hay un listado con todos los Widgets de panel que hay instalados.';
+$_lang['w_configcheck'] = 'Verificación de la configuración';
+$_lang['w_configcheck_desc'] = 'Muestra una verificación de la configuración que comprueba que su instalación de MODX sea segura.';
+$_lang['w_newsfeed'] = 'Fuente de Noticias de MODX';
+$_lang['w_newsfeed_desc'] = 'Muestra la Fuente de Noticias de MODX';
+$_lang['w_recentlyeditedresources'] = 'Recursos Editados Recientemente';
+$_lang['w_recentlyeditedresources_desc'] = 'Muestra un listado de los recursos editados más recientemente por el usuario.';
+$_lang['w_securityfeed'] = 'Fuente de Seguridad de MODX';
+$_lang['w_securityfeed_desc'] = 'Muestra la fuente de Seguridad de MODX';
+$_lang['w_whosonline'] = 'Quien está en linea';
+$_lang['w_whosonline_desc'] = 'Muestra un listado de usuarios en linea.';
